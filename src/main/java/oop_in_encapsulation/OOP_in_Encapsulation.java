92953542c4b196367be1f3e35b53d6eeee30f708
offic <offic@DESKTOP-GLF4UNU>
2024-07-10 11:36:57 +0600

    Welcome! Introduction to "OOP in Encapsulation".

---
+++ src/main/java/oop_in_encapsulation/OOP_in_Encapsulation.java
@@ -0,0 +1,9 @@
+package oop_in_encapsulation;
+
+public class OOP_in_Encapsulation {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome! Introduction to \"object oriented programming_OOP\".");
+    }
+    
+}
